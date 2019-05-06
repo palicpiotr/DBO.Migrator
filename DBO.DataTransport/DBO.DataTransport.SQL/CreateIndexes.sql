a173273ead5788ac24ff8e15f07505530a1e4cf1
Piotr Palic <petr.palic@altenar.com>
2019-05-06 20:49:08 +0300

    Configure environments and init SQL project for DBO Auth

---
+++ DBO.DataTransport/DBO.DataTransport.SQL/CreateIndexes.sql
@@ -0,0 +1,15 @@
+﻿  CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+
+  CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+
+  CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+
+  CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+
+  CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+  
+  CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+
+  CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+
+
---
+++ DBO.DataTransport/DBO.DataTransport.SQL/Migrations.sql
@@ -0,0 +1,3 @@
+﻿
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+       VALUES (N'00000000000000_CreateIdentitySchema', N'2.2.4-servicing-10062');
