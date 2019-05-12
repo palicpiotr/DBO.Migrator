d3c845f5cfde6603f4003c3048e9e022e566211d
Piotr Palic <petr.palic@altenar.com>
2019-05-12 13:42:21 +0300

    Prepare dbo instances

---
+++ DBO.DataTransport/DBO.DataTransport.DBOStore.DataModel/DesignTimeServices.cs
@@ -0,0 +1,14 @@
+﻿using DBO.DataTransport.DBOStore.DataModel.Pluralazing;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DBO.DataTransport.DBOStore.DataModel
+{
+    public class DesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IPluralizer, Pluralizer>();
+        }
+    }
+}
---
+++ DBO.DataTransport/DBO.DataTransport.DBOStore.DataModel/Pluralazing/Pluralizer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DBO.DataTransport.DBOStore.DataModel.Pluralazing
+{
+    public class Pluralizer : IPluralizer
+    {
+        public Pluralizer()
+        {
+            foreach (var item in _replacementItems)
+            {
+                _replacementBackItems[item.Value] = item.Key;
+            }
+        }
+
+        private static readonly Dictionary<string, string> _replacementBackItems = new Dictionary<string, string>();
+
+        private static readonly Dictionary<string, string> _replacementItems = new Dictionary<string, string>
+        {
+            {
+                "AspNetRoleClaims","AspNetRoleClaim"
+            },
+            {
+                "AspNetRoles","AspNetRole"
+            },
+            {
+                "AspNetUserClaims","AspNetUserClaim"
+            },
+            {
+                "AspNetUserLogins","AspNetUserLogin"
+            },
+            {
+                "AspNetUserRoles","AspNetUserRole"
+            },
+            {
+                "AspNetUsers","AspNetUser"
+            },
+            {
+                "AspNetUserTokens","AspNetUserToken"
+            },
+        };
+
+        public string Pluralize(string identifier) => _replacementBackItems.TryGetValue(identifier, out var cName) ? cName : identifier;
+
+        public string Singularize(string identifier) => _replacementItems.TryGetValue(identifier, out var cName) ? cName : identifier;
+    }
+}
